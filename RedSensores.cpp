87d1589e5423ec2d310b3c739283ffb9444bb83e
cvtorales <31779598+cvtorales@users.noreply.github.com>
2018-09-12 14:44:20 -0300

    Add files via upload
    Se agregaron comentarios con respecto al funcionamiento de RedSensores.
    Se corrigió el problema del ciclo para la primer linea (nombres de los sensores).
    No tocar el ultimo while, no cambiarlo a for.

--- RedSensores.cpp
+++ RedSensores.cpp
@@ -17,7 +17,7 @@ RedSensores::~RedSensores()
 RedSensores::RedSensores(istream& dss)
 {
 	string str, str_sensor;
-	int i=0, j=0;
+	int i=0, j=0, delimiter_pos = 0;
 	int initial_pos=0, final_pos=0, sensors_quantity=0;
 	Array<Sensor> sensores;
 	
@@ -27,21 +27,35 @@ RedSensores::RedSensores(istream& dss)
 
 	double number;
 
-	getline(dss, str);
-	//cout<<"str: "<<str<<endl;
-	cout << "Hola" << str[str.length()-1] << endl;
 
+
+ // Lectura y procesamiento de la primer linea (que contiene a los nombres de los sensores).
+ // Como son elementos separados por un delimitador, se decidio ubicar al delimitador para 
+ // obtener los nombres de cada sensor. En particular, para el ultimo nombre, se realizo una 
+ // comparacion con '\0' para poder obtenerlo.
+
+	getline(dss, str);
 
 	for(i=0; str[i] != '\0'; i++)
 	{	
-		if(str[i]==',')
+		if(str[i]==',' || str[i+1] == '\0')  // Para el ultimo nombre se compara con '\0'
 		{
 			Sensor s;
-			cout<<"initial_pos: "<<initial_pos<<endl;
-			cout<<"i: "<<i<<endl;
-			cout<<"nombre: "<<str.substr(initial_pos, i - initial_pos)<<endl;
 
-			s.SetName(str.substr(initial_pos, i - initial_pos));
+			if(str[i+1] == '\0')
+			{
+				delimiter_pos = i+1;   // Caso en que encuentra el ultimo nombre
+			}else
+			{
+				delimiter_pos =i;      // Caso en que encuentra el nombre por medio del delimitador
+			}
+
+			cout<<"initial_pos: "<<initial_pos<<endl;
+			cout<<"i: "<<delimiter_pos<<endl;
+			cout<<"nombre: "<<str.substr(initial_pos, delimiter_pos - initial_pos)<<endl;
+
+			s.SetName(str.substr(initial_pos, delimiter_pos - initial_pos));
+	
 			cout<<"nombre sensor: "<<s.GetName()<<endl;
 			sensores.Append(s);
 			cout<<"v[0]: "<<sensores[sensors_quantity].GetName()<<endl;
@@ -51,20 +65,12 @@ RedSensores::RedSensores(istream& dss)
 		}		
 	}
 
-	Sensor s;
-	
-	s.SetName(str.substr(initial_pos, i - initial_pos));
-	cout<<"nombre sensor: "<<s.GetName()<<endl;
-	
-	sensores.Append(s);
-	cout<<"v[0]: "<<sensores[sensors_quantity].GetName()<<endl;
-	sensors_quantity++;
-	cout<<"sensors_quantity: "<<sensors_quantity<<endl;
 
+// Lectura y procesamiento de los datos correspondientes a cada sensor, tener en cuenta 
+// que en cada linea se encuentran las posiciones de las columnas y la fila es constante
+// Se carga una fila a medida que se lee una linea.
 
-	cout << " " << endl;
-	
-	for(i=0 ; !dss.eof() ; i++)     // con este for recorro cada fila
+	for(i=0 ; !dss.eof() ; i++)     // Con este ciclo for se recorre cada fila
 	{          
 	      getline(dss, str);   // Con esto leemos cada linea y la asignamos a cadena
 	      initial_pos = 0;
@@ -72,32 +78,31 @@ RedSensores::RedSensores(istream& dss)
 	      j = 0;
 
 	      cout << "*************** Leo una linea ****************"<< endl;
-		  //cout<<"str: "<<str<<endl;
-	      
-	      while( j < sensors_quantity ) // con este for recorro cada sensor con la misma fila 
+
+	      while( j < sensors_quantity )  // con este ciclo recorro cada sensor con la misma fila 
 	      {
 			  	//cout<<"initial_pos: "<<initial_pos<<endl;
-		      		if(j != (sensors_quantity - 1))
-		      		{	
-			            final_pos = str.find(",", initial_pos) - initial_pos;
-			        }else
-			        {
-			         	final_pos = str.length() - initial_pos;
-			        }
-		            cout<<"final_pos: "<<final_pos<<endl;
-					str_sensor = str.substr(initial_pos, final_pos);
-					cout<< str_sensor <<endl;
-					// number = str_sensor.stod(str_sensor, &sz);
-					istringstream(str_sensor) >> number;
-					cout<<"jota: "<<j<<endl;
-					sensores[j].SetElementAt(number);
-					//datos.Append(number);
-		            //cout<<"datos["<<j<<"]: "<<datos[j]<<endl;
-		            initial_pos += final_pos+1;
-					cout<<initial_pos<<endl << endl;
-		            // convertir a str_sensor a double
-		            //sensores[j].SetData(datos);
-		            j++;
+	      		if(j != (sensors_quantity - 1))
+	      		{	
+		            final_pos = str.find(",", initial_pos) - initial_pos;
+		        }else
+		        {
+		         	final_pos = str.length() - initial_pos;
+		        }
+	            cout<<"final_pos: "<<final_pos<<endl;
+				str_sensor = str.substr(initial_pos, final_pos);
+				cout<< str_sensor <<endl;
+				// number = str_sensor.stod(str_sensor, &sz);
+				istringstream(str_sensor) >> number;
+				cout<<"jota: "<<j<<endl;
+				sensores[j].SetElementAt(number);
+				//datos.Append(number);
+	            //cout<<"datos["<<j<<"]: "<<datos[j]<<endl;
+	            initial_pos += final_pos+1;
+				cout<<initial_pos<<endl << endl;
+	            // convertir a str_sensor a double
+	            //sensores[j].SetData(datos);
+	            j++;
 
 
 	      }
