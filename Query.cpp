74a6f4af4a4dcb17f88ed6c5f3cb4ff521129fe0
Matías Jannello <mjannello@cnv.gov.ar>
2018-09-12 17:19:00 -0300

    Genero la carga de una Query a partir de una linea con N sensores

--- Query.cpp
+++ Query.cpp
@@ -1,5 +1,7 @@
 #include "Query.h"
 
+#define RANGO_INICIAL 0
+#define RANGO_FINAL 1
 
 using namespace std;
 
@@ -22,8 +24,7 @@ Query::Query(string line)
 	
 	int i=0;
 	int InitPos=0;
-	int QuantityChars=0;
-	string FirstValue, SecondValue;
+	
 
 	//cargo arreglo con las posiciones de las comas
 	while((int)line.find(",", InitPos) != -1)
@@ -41,11 +42,55 @@ Query::Query(string line)
 		InitPos = line.find(";", InitPos) + 1;
 	}
 
-	cout<<"Posicion de semicolons: "<<semicolons[0]<<endl;
-/*
-	FirstValue = line.substr(commas[0], commas[1]-commas[0]);
-	SecondValue= line.substr(commas[0], commas[1]-commas[0]);
-*/
-
+	if(semicolons.UsedSize() == 0)
+	{
+		this->SensorsName.Append(line.substr(0, commas[0]));
+	}else{
+		this->SensorsName = ObtieneNombreSensores(line,semicolons, commas);
+	}
+	
+	this->InitRange = ObtieneParametro(line,RANGO_INICIAL, commas);
+	this->FinalRange = ObtieneParametro(line,RANGO_FINAL, commas);
+	
+	
+	
+	
+		
 		
 }
+
+int Query::ObtieneParametro(string linea, int instruccion, Array<int>& array) const
+{
+	int ValorInstruccion=0;
+
+	if(instruccion < array.UsedSize()-1)
+	{
+	
+		istringstream(linea.substr(array[instruccion]+1, array[instruccion+1]-array[instruccion]-1)) >> ValorInstruccion;	
+		
+	}else{
+		istringstream(linea.substr(array[instruccion]+1, linea.length())) >> ValorInstruccion;	
+		
+	}
+	cout<<"ValorInstruccion: "<<ValorInstruccion<<endl;
+	return ValorInstruccion;
+}
+
+Array<string> Query::ObtieneNombreSensores(string linea, Array<int>& arraySemicolons, Array<int>& arrayCommas) const
+{
+	Array<string> NombreSensores;
+	int InitPos=0;
+
+	for(int i = 0; i<arraySemicolons.UsedSize() ;i++)
+	{
+
+		NombreSensores.Append(linea.substr(InitPos, arraySemicolons[i]-InitPos));
+		InitPos = arraySemicolons[i]+1;
+	}
+
+	NombreSensores.Append(linea.substr(InitPos, arrayCommas[0]-InitPos));
+
+	return NombreSensores;
+
+}
+
